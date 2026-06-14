5f7da2fd968c8ebe18bf6fca2728f6e1e87aa40b
Matt <matt@example.com>
2026-06-14 12:45:02 -0500

    Connect leaderboard backend

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,3 @@
 // Paste your deployed backend URL here, for example:
 // window.T6_BACKEND_URL = "https://t6-practice-api.onrender.com";
-window.T6_BACKEND_URL = "";
+window.T6_BACKEND_URL = "https://t6-study-leaderboard.mattsharkey143.workers.dev";
